51d609a2a571d75936cf8e391d3de0d4230547e2
Giga Begiashvili <108822024+Jadenco13@users.noreply.github.com>
2025-03-09 21:08:21 +0400

    feat: sign in (#4)
    * add auth services
    
    * add prettier refactor
    
    * add signin form with service
    
    * resolve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
 import { Component, signal } from '@angular/core';
-
-import {NavbarComponent} from './feature/navbar/navbar.component';
-import {LoginComponent} from './feature/login/login.component';
-import {NzContentComponent, NzHeaderComponent, NzLayoutComponent} from 'ng-zorro-antd/layout';
+import { NavbarComponent } from './feature/navbar/navbar.component';
+import { LoginComponent } from './feature/login/login.component';
+import {
+  NzContentComponent,
+  NzHeaderComponent,
+  NzLayoutComponent,
+} from 'ng-zorro-antd/layout';
+import { AuthService } from './core/services/auth.service';
+import { SignInDto } from './core/types/auth.types';
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent, LoginComponent, NzHeaderComponent, NzContentComponent, NzLayoutComponent],
+  imports: [
+    NavbarComponent,
+    LoginComponent,
+    NzHeaderComponent,
+    NzContentComponent,
+    NzLayoutComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   authorizationVisible = signal<boolean>(false);
   title = 'ng-online-shop';
-
+  constructor(public service: AuthService){}
   onSignIn() {
-    this.authorizationVisible.set(true)
+    this.authorizationVisible.set(true);
+  }
+  onClose() {
+    this.authorizationVisible.set(false);
+  }
+  onSubmit(userInfo: SignInDto) {
+    this.service.signIn(userInfo).subscribe(el => console.log(el))
   }
 }
