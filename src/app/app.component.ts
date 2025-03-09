ee18e49374b5f775f8ce62703890f2112c5afd66
giga begiashvili <giga.begi13@gmail.com>
2025-03-09 19:12:21 +0400

    add prettier refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,29 @@
 import { Component, signal } from '@angular/core';
-
-import {NavbarComponent} from './feature/navbar/navbar.component';
-import {LoginComponent} from './feature/login/login.component';
-import {NzContentComponent, NzHeaderComponent, NzLayoutComponent} from 'ng-zorro-antd/layout';
+import { NavbarComponent } from './feature/navbar/navbar.component';
+import { LoginComponent } from './feature/login/login.component';
+import {
+  NzContentComponent,
+  NzHeaderComponent,
+  NzLayoutComponent,
+} from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [ NavbarComponent, LoginComponent, NzHeaderComponent, NzContentComponent, NzLayoutComponent],
+  imports: [
+    NavbarComponent,
+    LoginComponent,
+    NzHeaderComponent,
+    NzContentComponent,
+    NzLayoutComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   authorizationVisible = signal<boolean>(false);
   title = 'ng-online-shop';
 
   onSignIn() {
-    this.authorizationVisible.set(true)
+    this.authorizationVisible.set(true);
   }
 }
