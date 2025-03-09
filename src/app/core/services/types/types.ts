f07dc46ee3d63acb56a802239921c150c41d154d
giga begiashvili <giga.begi13@gmail.com>
2025-03-09 17:39:34 +0400

    first step

---
+++ src/app/core/services/types/types.ts
@@ -0,0 +1,17 @@
+export interface SignIn {
+  email: string;
+  password: string;
+}
+
+export interface SignUp {
+  firstName: string;
+  lastName: string;
+  age: number;
+  email: string;
+  password: string;
+  address: string;
+  phone: string;
+  zipcode: string;
+  avatar: string;
+  gender: string;
+}
