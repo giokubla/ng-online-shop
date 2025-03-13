81c5dbc447aa286e455ee7fa34ba262b6f83d2c9
giga begiashvili <giga.begi13@gmail.com>
2025-03-13 15:19:12 +0400

    add pagination and remove breadcrum

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { ProductsService } from '../../core/services/products.service';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
-import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -9,38 +7,27 @@ import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { Product, ProductCategories } from '../../core/types/product.types';
 import { RouterModule } from '@angular/router';
-import { NzCascaderModule } from 'ng-zorro-antd/cascader';
-import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
-import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 
 @Component({
   selector: 'app-home',
   imports: [
-    NzBreadCrumbModule,
-    NzIconModule,
     NzMenuModule,
     NzLayoutModule,
     NzButtonModule,
     NzFlexModule,
     NzCardModule,
     RouterModule,
-    NzCascaderModule,
-    NzDatePickerModule,
-    NzGridModule,
-    NzInputModule,
-    NzInputNumberModule,
-    NzSelectModule,
+    NzPaginationModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  public productsLengthOnPage = signal<number>(0);
+  public productsLengthOnPage = signal<number>(10);
+  public pageIndex = signal<number>(1);
   public totalProducts = signal<number>(0);
-  public products = signal<Product[]>([]) ;
+  public products = signal<Product[]>([]);
   public productBrands = signal<string[]>([]);
   public productCategories = signal<ProductCategories[]>([]);
   constructor(public service: ProductsService) {
@@ -60,19 +47,23 @@ export class HomeComponent {
   }
   getProducts() {
     this.service
-      .products(5, 3)
+      .products(this.pageIndex(), this.productsLengthOnPage())
       .subscribe((el) => {
-        this.products.set(el.products)
-        this.totalProducts.set(el.total)
+        this.products.set(el.products);
+        this.totalProducts.set(el.total);
       });
   }
   getProductCategories() {
     this.service
       .productCategories()
-      .subscribe((el) => (this.productCategories.set(el)));
+      .subscribe((el) => this.productCategories.set(el));
   }
-  log() {
-    console.log(this.inp)
+  onPageSizeChange(size: number) {
+    this.productsLengthOnPage.set(size);
+    this.getProducts();
   }
-  inp = ''
+  onPageIndexChange(index: number) {
+    this.pageIndex.set(index);
+    this.getProducts();
+  }
 }
