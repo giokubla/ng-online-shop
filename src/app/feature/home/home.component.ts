1efcaee5669e156379c54287dd136bd9b65135b8
giga begiashvili <giga.begi13@gmail.com>
2025-03-13 14:38:52 +0400

    change properties to signal

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductsService } from '../../core/services/products.service';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzIconModule } from 'ng-zorro-antd/icon';
@@ -9,32 +9,70 @@ import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { Product, ProductCategories } from '../../core/types/product.types';
 import { RouterModule } from '@angular/router';
+import { NzCascaderModule } from 'ng-zorro-antd/cascader';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-home',
-  imports: [NzBreadCrumbModule, NzIconModule, NzMenuModule, NzLayoutModule, NzButtonModule, NzFlexModule, NzCardModule, RouterModule],
+  imports: [
+    NzBreadCrumbModule,
+    NzIconModule,
+    NzMenuModule,
+    NzLayoutModule,
+    NzButtonModule,
+    NzFlexModule,
+    NzCardModule,
+    RouterModule,
+    NzCascaderModule,
+    NzDatePickerModule,
+    NzGridModule,
+    NzInputModule,
+    NzInputNumberModule,
+    NzSelectModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  public products: Product[] = []
-  public productBrands: string[] = []
-  public productCategories: ProductCategories[] = []
+  public productsLengthOnPage = signal<number>(0);
+  public totalProducts = signal<number>(0);
+  public products = signal<Product[]>([]) ;
+  public productBrands = signal<string[]>([]);
+  public productCategories = signal<ProductCategories[]>([]);
   constructor(public service: ProductsService) {
-    this.getProductBrands()
-    this.getProducts()
-    this.getProductCategories()
+    this.getProductBrands();
+    this.getProducts();
+    this.getProductCategories();
   }
   getProductBrands() {
     this.service.productsBrands().subscribe({
-      next: (el) => {this.productBrands = el},
-      error: (err) => {err}
-    })
+      next: (el) => {
+        this.productBrands.set(el);
+      },
+      error: (err) => {
+        err;
+      },
+    });
   }
   getProducts() {
-    this.service.products(5, 3).subscribe(el => this.products = el.products)
+    this.service
+      .products(5, 3)
+      .subscribe((el) => {
+        this.products.set(el.products)
+        this.totalProducts.set(el.total)
+      });
   }
   getProductCategories() {
-    this.service.productCategories().subscribe(el => this.productCategories = el)
+    this.service
+      .productCategories()
+      .subscribe((el) => (this.productCategories.set(el)));
   }
+  log() {
+    console.log(this.inp)
+  }
+  inp = ''
 }
