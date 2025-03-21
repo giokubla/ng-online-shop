84354e9fd6f35b6b3ef2d08ac5cbb0a84785136d
giorgi kublashvili <giorgi.kublashvili2000@gmail.com>
2025-03-21 11:47:17 +0400

    refactor: add home page banner and fixed header

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,22 +1,30 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { BaseSearchDto, Category } from '../../core/types/product.types';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
-import { httpResource } from '@angular/common/http';
-import { buildParamsFromQuery } from '../../core/utils/query-params';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { NzEmptyComponent } from 'ng-zorro-antd/empty';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
-import { debounceTime } from 'rxjs';
 import { NzInputDirective } from 'ng-zorro-antd/input';
 import { NzDividerComponent } from 'ng-zorro-antd/divider';
 import { NzRateComponent } from 'ng-zorro-antd/rate';
+import {
+  NzFormControlComponent,
+  NzFormItemComponent,
+} from 'ng-zorro-antd/form';
+import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
+import { httpResource } from '@angular/common/http';
+import { BaseSearchDto, Category } from '../../core/types/product.types';
+import { buildParamsFromQuery } from '../../core/utils/query-params';
 
 @Component({
   selector: 'app-home',
@@ -35,6 +43,10 @@ import { NzRateComponent } from 'ng-zorro-antd/rate';
     NzInputDirective,
     NzDividerComponent,
     NzRateComponent,
+    NzFormItemComponent,
+    NzFormControlComponent,
+    NzColDirective,
+    NzRowDirective,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
@@ -42,32 +54,20 @@ import { NzRateComponent } from 'ng-zorro-antd/rate';
 export class HomeComponent {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private fb = inject(NonNullableFormBuilder);
   private routeQueryParams = toSignal(this.route.queryParams);
-  searchControl$ = new FormControl();
-  constructor() {
-    this.searchControl$.valueChanges
-      .pipe(
-        debounceTime(1000), // Wait 1 second after typing stops
-      )
-      .subscribe((value: string) => {
-        this.router.navigate([], {
-          queryParams: { keywords: value },
-          queryParamsHandling: 'merge',
-        });
-      });
-  }
   queryParams = computed(() => {
-    const params = this.routeQueryParams() || {};
     return {
-      page_size: Number(params['page_size']) || 10,
-      page_index: Number(params['page_index']) || 1,
-      brand: params['brand'],
-      keywords: params['keywords'],
-      category_id: params['category_id'],
+      page_size: +this.routeQueryParams()?.['page_size'] || 10,
+      page_index: +this.routeQueryParams()?.['page_index'] || 1,
+      rating: +this.routeQueryParams()?.['rating'] || 0,
+      brand: this.routeQueryParams()?.['brand'],
+      keywords: this.routeQueryParams()?.['keywords'],
+      category_id: this.routeQueryParams()?.['category_id'],
     };
   });
-  page_size = computed(() => this.queryParams().page_size);
-  page_index = computed(() => this.queryParams().page_index);
+  pageSize = computed(() => this.queryParams().page_size);
+  pageIndex = computed(() => this.queryParams().page_index);
   category = computed(() =>
     this.categories
       .value()
@@ -83,6 +83,18 @@ export class HomeComponent {
   categories = httpResource<Category[]>(
     () => 'https://api.everrest.educata.dev/shop/products/categories',
   );
+  filterForm = this.fb.group({
+    keywords: this.fb.control(''),
+    price_min: this.fb.control<number>(0),
+    price_max: this.fb.control<number>(0),
+    rating: this.fb.control<number>(0),
+  });
+  constructor() {
+    effect(() => {
+      const params = this.queryParams();
+      this.filterForm.patchValue(params);
+    });
+  }
 
   onPageSizeChange(page_size: number) {
     this.router.navigate([], {
@@ -98,7 +110,10 @@ export class HomeComponent {
     });
   }
 
-  onSearchInputChange($event: string) {
-    console.log($event);
+  onFormFilter() {
+    this.router.navigate([], {
+      queryParams: this.filterForm.getRawValue(),
+      queryParamsHandling: 'merge',
+    });
   }
 }
